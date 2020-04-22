3830efc5ffce287236853a53f5e3c83f33785106
Your Name <you@example.com>
2020-04-22 00:19:42 +0000

    fixed bugs in all methods and made sure in lowest terms

--- Fraction.java
+++ Fraction.java
@@ -48,17 +48,23 @@ public class Fraction {
     public Fraction add(Fraction n){
         int comd = n.getDenominator() * denominator;
         int ans = n.getDenominator()*numerator + n.getNumerator()*denominator;
-        return new Fraction(ans, comd);
+        Fraction b = new Fraction(ans, comd);
+        b.toLowestTerms();
+        return b;
     }
 
     public Fraction subtract(Fraction n){
         int comd = n.getDenominator() * denominator;
         int ans = n.getDenominator()*numerator - n.getNumerator()*denominator;
-        return new Fraction(ans, comd);
+         Fraction b = new Fraction(ans, comd);
+        b.toLowestTerms();
+        return b;
     }
 
     public Fraction multiply(Fraction n){
-        return new Fraction(n.getNumerator()*numerator, n.getDenominator()*denominator);
+        Fraction b = new Fraction(n.getNumerator()*numerator, n.getDenominator()*denominator);
+        b.toLowestTerms();
+        return b;
     }
 
     public Fraction divide(Fraction n){
@@ -66,33 +72,35 @@ public class Fraction {
             double i = (numerator * n.getDenominator()) / (denominator  * n.getNumerator());
         }
         catch (Exception e){
-            return new Fraction();
+            throw new IllegalArgumentException("Cannot divide by zero");
         }
-        return new Fraction(numerator * n.getDenominator(), denominator  * n.getNumerator());
+        Fraction b = new Fraction(numerator * n.getDenominator(), denominator  * n.getNumerator());
+        b.toLowestTerms();
+        return b;
     }
 
     public boolean equals(Object n){
         Fraction b = (Fraction) n;
-        // n.toLowestTerms();
+        b.toLowestTerms();
         if (b.getNumerator() == numerator && b.getDenominator() == denominator){
             return true;
         }
         return false;
     }
-    
+
     public void toLowestTerms(){
         int gcd = gcd(numerator, denominator);
         numerator = numerator / gcd;
         denominator = denominator / gcd;
     }
-    
-    static int gcd(int n, int d){  
-    if (d == 0){  
+
+    static int gcd(int n, int d){
+    if (d == 0){
         return n;
     }
-    return gcd(d, n % d);  
-      
-    } 
+    return gcd(d, n % d);
+
+    }
 
 
 
