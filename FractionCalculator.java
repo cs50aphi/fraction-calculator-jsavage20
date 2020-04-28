e3fdbafb91f511c373381047115fec5b5f485a5f
Your Name <you@example.com>
2020-04-28 05:46:31 +0000

    bare bones greeting

---
+++ FractionCalculator.java
@@ -0,0 +1,16 @@
+import java.util*;
+
+public class FractionCalculator{
+
+    public static void main(String[], args){
+        Scanner kb = new Scanner(System.in);
+        boolean quit = false;
+
+        System.out.println("Welcome to the Fraction Calculator!");
+        System.out.println("It will add, subtract, multiply, and divide fractions until you type Q to quit.");
+        System.out.println("Please enter your fractions in the form a/b where a and b are both integers.");
+        while (!quit){
+            System.in.println("(+, -, *, /, = or Q to Quit):")
+        }
+    }
+}
